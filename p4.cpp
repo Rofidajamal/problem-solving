551a553cfe5160d7ee7f21add1d88c88606ea861
Rofidajamal <99247792+Rofidajamal@users.noreply.github.com>
2022-02-08 12:40:57 +0200

    Add files via upload

---
+++ p4.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = int(5e3) + 9;
+
+int n;
+int a[N];
+int dp[N][N];
+
+int calc (int pos, int x) {
+    int &res = dp[pos][x];
+    if (res != -1) return res;
+
+    if (pos == n) return res = 0;
+    res = 1 + calc(pos + 1, n);
+    res = min(res, calc(pos + 1, pos) + a[pos]);
+    if (x != n) {
+        if (a[x] >= a[pos])
+            res = min(res, calc(pos + 1, pos));
+        else {
+            res = min(res, calc(pos + 1, pos) + a[pos] - a[x]);
+            res = min(res, 1 + calc(pos + 1, x));
+        }
+    }
+
+    return res;
+}
+
+int main(){
+    cout<<"enter the number of elements \n";
+    cin>>n;
+    int x;
+    cout<<"Enter you elements \n";
+    for (int i = 0; i < n; ++i)
+       {cin>>x;
+        a[i]=x;
+       }
+
+    memset(dp, -1, sizeof(dp));
+    cout<<"the output: "<< calc(0, n)<<endl;
+
+    return 0;
+}
