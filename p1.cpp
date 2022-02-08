ba9c55184c96b192dab58fd8f98575a9007c240a
Rofidajamal <99247792+Rofidajamal@users.noreply.github.com>
2022-02-08 12:40:04 +0200

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+int InsertionSort (int arr [],int size_)
+{
+    int key , j ,comp=0;
+    for (int i =1 ;i<size_;i++)
+    {
+        key = arr[i]; //second element in the array
+        j = i -1; // first element
+        while(j>=0 && arr[j]>key)
+            {
+                comp++; //to calculate the number of comparisons
+                arr[j+1]= arr[j]; //shifting to the left
+                j =j-1;
+            }
+            arr [j+1]= key; // putting the element in the proper place
+
+
+    }
+    return comp;
+}
+int main()
+{
+    int n;
+    cout<<"enter the number of elements \n";
+    cin>>n;
+    int A [n] ; //{2,1,3,1,2}
+    int c = 0;
+    cout<<"Enter the elements \n";
+    for (int i = 0;i<n;i++){
+        cin>>A[i];
+    }
+    c = InsertionSort(A,n);
+    cout<<"the number of comparisons:  "<<c<<endl;
+    cout<<"the array after sorting : ";
+    for (int i =0;i<5;i++)
+    {
+        cout<<A[i] <<" ";
+    }
+
+
+    return 0;
+}
